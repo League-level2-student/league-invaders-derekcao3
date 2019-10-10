ea1006a1addb58dd0dce4737db5761695f7c35dd
SMHS-519-19 <SMHS-519-19@SMHS-519-19.smhsrop.lab>
2019-10-10 16:20:33 -0700

    League Invaders Part 1

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,22 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	final int width = 500;
+	final int height = 800;
 
+	public static void main(String[] args) {
+		LeagueInvaders game = new LeagueInvaders();
+		game.setup();
+	}
+
+	LeagueInvaders() {
+		frame = new JFrame();
+	}
+
+	public void setup() {
+		frame.show();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(width, height);
+	}
 }
