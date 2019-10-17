800a62f4c1eefab540a3931c442241afdda58617
SMHS-519-19 <SMHS-519-19@SMHS-519-19.smhsrop.lab>
2019-10-17 15:18:21 -0700

    League Invaders Step 2(Complete)

---
+++ src/GamePanel.java
@@ -0,0 +1,23 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener{  
+Timer timer;
+
+
+GamePanel(){
+	timer = new Timer(1000/60, this);
+}
+void startGame() {
+	timer.start();
+}
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	System.out.println("yes");
+}
+
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,9 +1,12 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
 	JFrame frame;
 	final int width = 500;
 	final int height = 800;
+	GamePanel GamePanel;
 
 	public static void main(String[] args) {
 		LeagueInvaders game = new LeagueInvaders();
@@ -12,11 +15,18 @@ public class LeagueInvaders {
 
 	LeagueInvaders() {
 		frame = new JFrame();
+		GamePanel = new GamePanel();
 	}
 
 	public void setup() {
-		frame.show();
+		frame.add(GamePanel);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(width, height);
+		frame.getContentPane().setPreferredSize(new Dimension(width, height));
+		frame.pack();
+		frame.setVisible(true);
+		GamePanel.startGame();
 	}
+	
 }
