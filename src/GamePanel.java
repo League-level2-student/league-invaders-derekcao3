d82410779cc5e366711459dc5a770c464d68bd82
SMHS-519-19 <SMHS-519-19@SMHS-519-19.smhsrop.lab>
2019-10-17 15:22:45 -0700

    League Invaders Level 3 (Completed)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,23 +1,32 @@
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener{  
-Timer timer;
+public class GamePanel extends JPanel implements ActionListener {
+	Timer timer;
 
+	GamePanel() {
 
-GamePanel(){
-	timer = new Timer(1000/60, this);
-}
-void startGame() {
-	timer.start();
-}
-@Override
-public void actionPerformed(ActionEvent arg0) {
-	// TODO Auto-generated method stub
-	System.out.println("yes");
-}
+		timer = new Timer(1000 / 60, this);
+	}
+
+	@Override
+
+	public void paintComponent(Graphics g) {
+		 g.fillRect(10, 10, 100, 100);
+	}
+
+	void startGame() {
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
 
 }
