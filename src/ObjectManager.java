73c10e4a9818d0c71f8cdb72795fb70fa909db3c
SMHS-519-19 <SMHS-519-19@SMHS-519-19.smhsrop.lab>
2019-11-07 16:29:12 -0800

    League Invaders Part 11 (unfinished)

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -2,26 +2,43 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class ObjectManager {
-Rocketship rocketship;
-ObjectManager(Rocketship rocketship){
-this.rocketship = rocketship;
-}
-ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	Rocketship rocketship;
 
-void addProjectile(Projectile projectile) {
-	projectiles.add(projectile);
-}
-void update() {
-	rocketship.update();
-	for (Projectile p:projectiles){
-		p.update();
+	ObjectManager(Rocketship rocketship) {
+		this.rocketship = rocketship;
 	}
-}
-void draw(Graphics g) {
-rocketship.draw(g);
-for (Projectile p:projectiles){
-	p.draw(g);
-}
-}
+
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+
+	void addProjectile(Projectile projectile) {
+		projectiles.add(projectile);
+	}
+
+	void update() {
+		rocketship.update();
+		for (Projectile p : projectiles) {
+			p.update();
+		}
+		for (Alien p : aliens) {
+			p.update();
+		}
+	}
+
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+
+	void addAlien(Alien alien) {
+		aliens.add(alien);
+	}
+
+	void draw(Graphics g) {
+		rocketship.draw(g);
+		for (Projectile p : projectiles) {
+			p.draw(g);
+		}
+		for (Alien p : aliens) {
+			p.draw(g);
+		}
+
+	}
 
 }
